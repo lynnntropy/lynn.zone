95075499ae95b56cdda9776cb467f96f7fd510b5
Lynn Romich <lynn@lynn.zone>
2025-10-06 18:39:09 +0200

    feat: convert `Nav` to Astro component
    (and extract Rin to her own Svelte component)

--- src/components/Rin.svelte
+++ src/components/Rin.svelte
@@ -1,13 +1,6 @@
 <script lang="ts">
   import type { Action } from "svelte/action";
 
-  const { style = "default" } = $props();
-
-  const links = [
-    { href: "/", text: "Home" },
-    { href: "/blog", text: "Blog" },
-  ];
-
   let prefersReducedMotion: boolean;
 
   $effect(() => {
@@ -47,28 +40,9 @@
   };
 </script>
 
-<nav
-  class={[
-    "mb-12 w-full self-center flex items-center gap-8 sm:mb-24 2xl:fixed 2xl:left-8 2xl:flex-col 2xl:items-stretch 2xl:w-48",
-    style === "default" && "max-w-3xl",
-    style === "blog-post" && "max-w-2xl",
-  ]}
->
-  <a href="/">
-    <img
-      use:rotate
-      alt="Go to homepage"
-      src="/images/avatar.png"
-      class="size-12 rounded-full 2xl:size-16 2xl:mx-0 2xl:mb-4"
-    />
-  </a>
-  <ul class="flex gap-4 text-lg 2xl:flex-col 2xl:text-xl">
-    {#each links as { href, text }}
-      <li>
-        <a {href} class="hover:underline">
-          {text}
-        </a>
-      </li>
-    {/each}
-  </ul>
-</nav>
+<img
+  use:rotate
+  alt="Go to homepage"
+  src="/images/avatar.png"
+  class="size-12 rounded-full 2xl:size-16 2xl:mx-0 2xl:mb-4"
+/>
